70b5cbe5a45a89d91864f7483bef9164ddc4f187
cleoncheng2000 <88440925+cleoncheng2000@users.noreply.github.com>
2022-06-09 17:01:36 +0800

    dialogue multiple names add (Bug)

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
-
-    public string name;
+    [TextArea(3, 10)]
+    public string[] names;
 
     [TextArea(3,10)]
     public string[] sentences;
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -11,11 +11,13 @@ public class DialogueManager : MonoBehaviour
 
     public Animator animator;
 
+    private Queue<string> names;
     private Queue<string> sentences;
 
     // Start is called before the first frame update
     void Start()
     {
+        names = new Queue<string>();
         sentences = new Queue<string>();
     }
 
@@ -24,18 +26,34 @@ public class DialogueManager : MonoBehaviour
 
         animator.SetBool("IsOpen", true);
 
-        nameText.text = dialogue.name;
-
+        names.Clear();
         sentences.Clear();
 
-        foreach(string sentence in dialogue.sentences)
+        foreach (string name in dialogue.names)
+        {
+            sentences.Enqueue(name);
+        }
+
+        foreach (string sentence in dialogue.sentences) 
         {
             sentences.Enqueue(sentence);
         }
 
+        DisplayNextName();
         DisplayNextSentence();
     }
 
+    public void DisplayNextName()
+    {
+        if (names.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string name = names.Dequeue();
+        nameText.text = name;
+    }
     public void DisplayNextSentence () 
     {
        if (sentences.Count == 0)
@@ -48,6 +66,7 @@ public class DialogueManager : MonoBehaviour
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
     }
+
 
     IEnumerator TypeSentence (string sentence)
     {
