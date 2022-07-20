82892eba2358382a06a9a35eef4719d78bd01387
GohJW <aarongjw00@gmail.com>
2022-07-21 02:50:51 +0800

    save

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     public Slider slider;
     public AudioManager AudioManager;
+    public Button Stagebutton;
+    public TMP_Text Stagebuttontext;
+
+    public void Start()
+    {
+        AudioManager = FindObjectOfType<AudioManager>();
+        if(AudioManager.Stage2Played == true)
+        {
+            Stagebutton.interactable = true;
+            Stagebuttontext.color = new Color(1, 1, 1, 1);
+        }
+    }
 
     public void PlayGame()
     {
         FindObjectOfType<AudioManager>().Play("ButtonClick");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       
     }
 
     public void QuitGame()
